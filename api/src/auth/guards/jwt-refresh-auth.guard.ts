60fa2130a6f160aa4d6f85c1b914890d5c70bd91
Dragonaft <supperator9@gmail.com>
2026-05-14 21:07:13 +0200

    fix: cookies refresh guard clears

--- api/src/auth/guards/jwt-refresh-auth.guard.ts
+++ api/src/auth/guards/jwt-refresh-auth.guard.ts
@@ -1,5 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
-export class JwtRefreshAuthGuard extends AuthGuard('jwt-refresh') {}
+export class JwtRefreshAuthGuard extends AuthGuard('jwt-refresh') {
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    try {
+      return (await super.canActivate(context)) as boolean;
+    } catch (error) {
+      const response = context.switchToHttp().getResponse();
+      response.clearCookie('access_token');
+      response.clearCookie('refresh_token');
+      throw error;
+    }
+  }
+}
